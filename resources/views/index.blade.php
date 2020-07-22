cb72ee75070cf4448b677810971491b54fefb8ea
wright <calebogundiya@gmail.com>
2020-07-22 04:19:27 +0100

    Configure icons and finish Header section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="flex bg-gray-300 rounded-b-lg">
-			<div class="pl-5 w-1/3">
-				<h2 class="my-5 text-4xl font-bold mx-10">employee</h2>
-				<div class="flex flex-col bg-white ml-5 p-10 shadow-2xl sticky rounded-lg text-xl font-bold">
-					<h3>
-						<img
-                        src=""
-                        class="inline mr-5 rounded-full h-16 w-16 items-center justify-center"
-                    />All Employees</h3>
-
-					<h5 class="text-gray-500 text-xl mt-10">PROJECT</h5>
-
-					<div class="rounded-md py-5"><img src="https://placeimg.com/30/30/any" class="rounded-full h-12 w-12 items-center justify-center inline"> Arena Sport</div>
-						
-					<div class="rounded-md px-5 py-3 bg-gray-300 br-5"><img src="https://placeimg.com/30/30/tech" class="rounded-full h-12 w-12 items-center justify-center inline"> Arena Sport</div>					
-
-					<div class="rounded-md py-5"><img src="https://placeimg.com/30/30/arch" class="rounded-full h-12 w-12 items-center justify-center inline"> Arena Sport</div>	
-								
-					<div class="rounded-md py-5"><img src="https://placeimg.com/30/30/nature" class="rounded-full h-12 w-12 items-center justify-center inline"> Arena Sport</div>				
-                        <button type="submit" class="br-5 py-2 px-5 bg-purple-500 absolute rounded-full bottom-0">add project</button>
-				</div>
-			</div>
-		</div>
+    Gigs
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,56 +1,77 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+	<title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+	<!-- Scripts -->
+	<script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/7e62766cf7.js" crossorigin="anonymous"></script>
+	<!-- Fonts -->
+	<link rel="dns-prefetch" href="//fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/7e62766cf7.js" crossorigin="anonymous"></script>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+	<!-- Styles -->
+	<link href="{{ asset('css/main.css') }}" rel="stylesheet">
+	<style>
+		body {
+			font: Regular 45px/63px Circular Std Bold;
+		}
+	</style>
+
 </head>
+
 <body>
-    
-    <div id="app" class="bg-gray-300 font-sans">
-        <div class="container shadow-2xl rounded-lg text-gray-700">
-		<div class="py-6 px-4 flex justify-between bg-white rounded-t-lg">
+
+	<div id="app" class="bg-gray-300 font-sans flex h-screen">
+		<div class="w-1/6 pl-6 bg-gray-200" style="border: 1px solid #ECECEC; opacity: 0.5; font-family: Nunito">
 			<div>
-				<img src="" class="inline mx-5 rounded-full h-16 w-16 items-center justify-center"/>
-				<p class="inline font-bold text-4xl">Internia</p>
+				<img src="{{asset('img/Group493.svg')}}" class="h-20 w-20 items-center justify-center inline">
 			</div>
-			<div class="flex content-center font-medium text-gray-600 my-auto">
-				
+
+			<div class="py-5">
+				<img src="{{asset('img/Icon-home-fill.svg')}}" class="h-6 w-6 items-center justify-center inline"> Dashboard
 			</div>
-			<div class="float-right">
-				<div class="flex">
-					<p  class="mx-5 mt-2 text-3xl">
-						<i class="far fa-bell"></i>
-					</p>
-					<p  class="mx-5 mt-2 text-3xl">
-						<i class="far fa-envelope"></i>
-					</p>
-					<img src="https://i.pravatar.cc/50?u=xf" class="inline mx-5 rounded-full h-8 w-8 items-center justify-center"/>
-				</div>
+
+			<div class="py-5">
+				<img src="{{asset('img/Icon-briefcase-fill.svg')}}" class="h-6 w-6 items-center justify-center inline"> Gigs
+			</div>
+
+			<div class="py-5">
+				<img src="{{asset('img/Icon-home-fill.svg')}}" class="h-6 w-6 items-center justify-center inline"> Company
+			</div>
+
+			<div class="py-5">
+				<img src="{{asset('img/Icon-home-fill.svg')}}" class="h-6 w-6 items-center justify-center inline"> Account
 			</div>
 		</div>
 
-		
+		<div class="w-5/6 bg-gray-200">
+			<header class="justify-between py-6 px-16 flex" style="border: 1px solid #ECECEC; opacity: 1;">
+				<div class="w-2/3">
+					<input type="search" name="" id="" class="p-2 w-3/4 bg-gray-100 border-2 border-gray-200 rounded-lg" placeholder="Search">
+				</div>
+				<div>
+					<img src="{{asset('img/Group140.svg')}}" class="h-4 w-4 mr-12 items-center justify-center inline">
+					<img src="{{asset('img/Icon-email.svg')}}" class="h-4 w-4 mr-12 items-center justify-center inline">
+					<img src="{{asset('img/Icon-settings.svg')}}" class="h-4 w-4 mr-12 items-center justify-center inline">
+					<img src="{{asset('img/Rectangle13.png')}}" class="h-6 w-6 items-center justify-center inline">
+				</div>
+			</header>
+			<main class="px-16">
+				@yield('content')
 
-
-            @yield('content')
+			</main>
+		</div>
 
 	</div>
-    </div>
 </body>
+
 </html>
