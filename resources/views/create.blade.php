a3c5f9db7e717d727a29a5f4e851e645bdf3fb10
wright <calebogundiya@gmail.com>
2020-07-22 14:16:39 +0100

    finished index heading

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -12,15 +12,53 @@
 		<h1>New gig</h1>
 	</div>
 	<div class="flex justify-between">
-		<div class="gigMenu">
+		<div class="gigMenu w-1/4">
 			<div>Basic Data</div>
 			<div>Remuneration</div>
 		</div>
-		<div class="gigOptions">
+		<div class="gigOptions w-3/4">
 			<h1>Salary</h1>
 			<div class="flex justify-between">
-				<input type="text" placeholder="Minimum">
-				<input type="text" placeholder="Minimum">
+				<div>
+					<input type="text" placeholder="Minimum">
+				</div>
+				<div>
+					<input type="text" placeholder="Minimum">
+				</div>
+			</div>
+			<div>
+				<div class="flex mb-4">
+					<div>
+						<label for="role">Role</label>
+						<input type="text" id="role" value="Product Designer">
+					</div>
+					<div>
+						<label for="role">Company</label>
+						<input type="text" value="TM30">
+					</div>
+				</div>
+
+				<div class="mb-4">
+					<p>Location</p>
+					<div class="flex">
+						<input type="text" placeholder="Country">
+						<input type="text" placeholder="State/Region">
+					</div>
+					<input type="text" placeholder="Address">
+				</div>
+
+				<div class="mb-4">
+					<p>Add Tags</p>
+					<div>
+						<input type="text" placeholder="Minimum">
+					</div>
+					Suggested tags: <span class="underline">full time</span> <span class="underline">Contract</span> <span class="underline">freelance</span>
+				</div>
+
+				<div class="mb-4 justify-end">
+					<a href="/">Cancel</a>
+					<button>Continue</button>
+				</div>
 			</div>
 		</div>
 	</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,55 +2,81 @@
 
 @section('content')
 <div>
-    <div class="px-16 flex justify-between">
+    <div class="px-16 py-2 pb-8 mb-2 flex justify-between items-center">
         <div>
-            <h1>Gigs</h1>
+            <h1 class="text-3xl text-gray-700 font-bold">Gigs</h1>
         </div>
 
         <div>
-            <button type="submit">New Gig +</button>
+            <a href="/create" class="text-white text-xs bg-purple-900 rounded-md p-2">New Gig +</a>
         </div>
     </div>
-    <div class="px-16 flex justify-between">
-        <div>
-            <h1>All gigs <span>3404</span></h1>
+    <div class="px-16 mb-2 font-bold text-gray-600 text-sm flex justify-between border-b-2 border-gray-300">
+        <div class="border-b-2 border-orange-400 w-1/5">
+            <h1 class="pb-4">
+                <a href="#">All gigs <span class=" bg-orange-400 px-3 text-white text-xs rounded-md">3404</span></a>
+            </h1>
         </div>
 
-        <div>
-            <h1>My gigs <span>1200</span></h1>
+        <div class=" w-1/5">
+            <h1 class="pb-4">
+                <a href="#">My gigs <span class=" bg-orange-400 px-3 text-white text-xs rounded-md">1200</span></a>
+            </h1>
         </div>
 
-        <div>
-            <h1>Rejected gigs <span>100</span></h1>
+        <div class=" w-1/5">
+            <h1 class="pb-4">
+                <a href="#">Rejected gigs <span class=" bg-orange-400 px-3 text-white text-xs rounded-md">100</span></a>
+            </h1>
         </div>
     </div>
-    <div class="px-16 flex justify-between">
-        <div>
-            <button type="submit">Freelance</button>
+    <div class="px-16 py-8 flex justify-between justify- text-gray-700">
+        <div class="w-1/6">
+            <button type="submit" class="border-2 border-gray-300 py-1 px-3 rounded-lg">
+                Freelance
+            </button>
         </div>
 
-        <div>
-            <button type="submit">Keywords</button>
+        <div class="w-1/6">
+            <button type="submit" class="border-2 border-gray-300 py-1 px-3 rounded-lg">
+                <img src="{{asset('img/Icon-move.svg')}}" class="h-4 w-4 inline">
+                Keywords
+                &or;
+            </button>
         </div>
 
-        <div>
-            <button type="submit">Location</button>
+        <div class="w-1/6">
+            <button type="submit" class="border-2 border-gray-300 py-1 px-3 rounded-lg">
+                <img src="{{asset('img/Icon-pin.svg')}}" class="h-4 w-4 inline">
+                Location
+                &or;
+            </button>
         </div>
 
-        <div>
-            <button type="submit">Remote Friendly</button>
+        <div class="w-1/6">
+            <button type="submit" class="border-2 border-gray-300 py-1 px-3 rounded-lg">
+                <img src="{{asset('img/Icon-globe.svg')}}" class="h-4 w-4 inline">
+                Remote Friendly
+            </button>
         </div>
 
-        <div>
-            <button type="submit">Design</button>
+        <div class="w-1/6">
+            <button type="submit" class="border-2 text-orange-400 border-orange-400 py-1 px-3 rounded-lg">
+                <img src="{{asset('img/Icon-color-palette.svg')}}" class="h-4 w-4 inline">
+                Design
+                &check;
+            </button>
         </div>
 
-        <div>
-            <button type="submit">Contract</button>
+        <div class="w-1/6">
+            <button type="submit" class="border-2 border-gray-300 py-1 px-3 rounded-lg">
+                <img src="{{asset('img/Icon-briefcase.svg')}}" class="h-4 w-4 inline">
+                Contract
+            </button>
         </div>
     </div>
-    <div class="px-16">
-        <table>
+    <div class="px-16 flex">
+        <table class=" w-full">
             <thead>
                 <th></th>
                 <th>Role</th>
@@ -60,13 +86,15 @@
                 <th></th>
             </thead>
             <tbody>
-                <tr><input type="checkbox" name="" id=""></tr>
-                <tr>Product Designer</tr>
-                <tr>TM30</tr>
-                <tr>20th June, 2020</tr>
-                <tr>20,000 - 30,000</tr>
                 <tr>
-                    <button type="submit">Delete</button></tr>
+                    <td><input type="checkbox" name="" id=""></td>
+                    <td>Product Designer</td>
+                    <td>TM30</td>
+                    <td>20th June, 2020</td>
+                    <td>20,000 - 30,000</td>
+                    <td>
+                        <button type="submit">Delete</button></td>
+                </tr>
             </tbody>
         </table>
     </div>
