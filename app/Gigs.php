755af235cbf950567a0ff8236eb4eb72e447014f
wright <calebogundiya@gmail.com>
2020-07-23 13:49:58 +0100

    Carbon timee

--- app/Gigs.php
+++ app/Gigs.php
@@ -3,9 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Gigs extends Model
 {
     //
     protected $guarded = [];
+
+    public function getCreatedAtAttribute($value)
+    {
+        $dt = Carbon::createFromDate($value);
+
+        return $dt->diffForHumans();
+    }
 }
